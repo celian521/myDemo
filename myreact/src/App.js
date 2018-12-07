3f5bc83c496e4d62a81549d4e1399ce23a7003e7
celian521 <celian521@163.com>
2018-12-07 11:18:02 +0800

    no message

--- myreact/src/App.js
+++ myreact/src/App.js
@@ -5,7 +5,7 @@ import './assets/style/a.scss';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from './components/Home.js';
 import Home2 from './components/Home2.js';
-import mobx from './components/mobx.js';
+
 
 class App extends Component {
   render() {
@@ -15,7 +15,6 @@ class App extends Component {
           <Link to="/h/555?k=2000">link</Link>
           <Route exact path="/" component={Home} />
           <Route path="/h/:aid" component={Home2} />
-          <Route path="/mobx/" component={mobx} />
         </div>
       </Router>
     );

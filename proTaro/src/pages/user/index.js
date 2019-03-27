822b640a88ad777cfcac9a78733975666e66c940
celian521 <celian521@163.com>
2019-03-27 18:00:54 +0800

    no message

--- proTaro/src/pages/user/index.js
+++ proTaro/src/pages/user/index.js
@@ -1,6 +1,6 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View } from '@tarojs/components'
-import { AtButton } from 'taro-ui'
+import { View, Image } from '@tarojs/components'
+import { AtDivider } from 'taro-ui'
 
 import './index.scss'
 
@@ -25,9 +25,33 @@ class Index extends Component {
   render () {
     const { } = this.props
     return (
-      <View className='index'>
-        <AtButton loading type='primary'>按钮=文案</AtButton>
+
+<View className='at-article'>
+  <View className='at-article__h1'>
+    这是一级标题这是一级标题
+  </View>
+  <View className='at-article__info'>
+    2017-05-07&nbsp;&nbsp;&nbsp;这是作者
+  </View>
+  <AtDivider content='分割线' />
+  <View className='at-article__content'>
+    <View className='at-article__section'>
+      <View className='at-article__h2'>这是二级标题</View>
+      <View className='at-article__h3'>这是三级标题</View>
+      <View className='at-article__p'>
+        这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本段落。这是文本落。这是文本段落。1234567890123456789012345678901234567890 ABCDEFGHIJKLMNOPQRSTUVWXYZ
       </View>
+      <View className='at-article__p'>
+        这是文本段落。这是文本段落。
+      </View>
+      <Image
+        className='at-article__img'
+        src='https://dummyimage.com/750x400/ccc/f0f'
+        mode='widthFix'
+      />
+    </View>
+  </View>
+</View>
     )
   }
 }

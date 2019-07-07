a5d85ada57e79ac3bed4632a56c7911c55af2c1b
DESKTOP-G5ADAQG\stephen <celian521@163.com>
2019-07-07 17:57:23 +0800

    no message

--- bupa-gulp/src/js/main.js
+++ bupa-gulp/src/js/main.js
@@ -8,11 +8,11 @@ Vue.filter('sexMap', function (value) {
   return a[value || 0]
 })
 
-Vue.component('button-counter', {
-  data: function () {
-    return {
-      count: 0
-    }
-  },
-  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
-})
+// Vue.component('button-counter', {
+//   data: function () {
+//     return {
+//       count: 0
+//     }
+//   },
+//   template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+// })
